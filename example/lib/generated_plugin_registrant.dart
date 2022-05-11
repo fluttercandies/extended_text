b15033d1d8b21dfb5b37c1b3e168288385ea61a7
zmtzawqlp <zmtzawqlp@live.com>
2022-05-12 01:53:21 +0800

    # 9.1.0 * Migrate to 3.0.0

--- example/lib/generated_plugin_registrant.dart
+++ example/lib/generated_plugin_registrant.dart
@@ -11,7 +11,7 @@ import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
-void registerPlugins(final Registrar registrar) {
+void registerPlugins(Registrar registrar) {
   UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
